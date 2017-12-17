85a156d1cab362e9bb52370485b1c2d89fa4382e
sonukumar <skbhagat.nita@gmail.com>
2017-12-17 16:52:02 +0530

    New project for spring batch

---
+++ spring-boot-core/src/main/java/spring_boot_core/spring_boot_core/App.java
@@ -0,0 +1,13 @@
+package spring_boot_core.spring_boot_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
